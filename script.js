54d41eaca883b0c269db86c7584b30a5e15a58fc
Luis Felipe Bastidas <230185135+luisfelipebastidas@users.noreply.github.com>
2026-09-04 20:45:48 -0700

    Create function for computer choice

---
+++ script.js
@@ -0,0 +1,25 @@
+// What's the problem?
+// 1- A function that returns randomly “rock”, “paper” o “scissors”.
+// 2- An input for the user play
+// 3-
+
+// BREAK IT DOWN
+// como hacer que aparezcan valores random? ----> math.random
+// como convertirlos a strigs? con if/else
+// how to return one of those values? return dentro de if/ else
+
+// how to create an input for the user? -----> prompt?
+// what to do with that value? put it in a variable
+// How to compare results?
+
+function getComputerChoice() {
+  const randomNumber = Math.random() * 3;
+  if (randomNumber >= 0 && randomNumber < 1) {
+    return "Rock";
+  } else if (randomNumber >= 1 && randomNumber < 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+console.log(getComputerChoice());
